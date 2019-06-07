6cf03873ea61af9f673fdb9efd6332a103211120
Freddy Snijder <freddy@visionscapers.com>
2019-06-07 18:17:11 +0200

    Package setup fix

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='keras_callbacks',
     version='0.3',
-    packages=['keras_callbacks'],
+    packages=find_packages(),
     license='MIT license',
     long_description=open('README.md').read(),
     dependency_links=['git+https://github.com/visionscaper/pybase.git'],
-    install_requires=[
-        'keras>=2.1.6'
-    ])
+    install_requires=[])
